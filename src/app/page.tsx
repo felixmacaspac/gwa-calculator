7ba2d8301e19279226723dddf643e38fd015a9c9
felixmacaspac <fmcmacaspac@protonmail.com>
2024-01-09 21:09:15 +0800

    feat: initial gwa calculator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,175 @@
-import Image from 'next/image'
+"use client";
+import { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import {
+  ExclamationTriangleIcon,
+  PlusCircledIcon,
+  MinusCircledIcon,
+} from "@radix-ui/react-icons";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+interface Grade {
+  subject: string;
+  grade: number | string;
+  units: number | string;
+}
 
 export default function Home() {
+  const [grades, setGrades] = useState<Grade[]>(() => {
+    const storedGrades = localStorage.getItem("grades");
+    if (storedGrades) {
+      return JSON.parse(storedGrades);
+    } else {
+      return [
+        {
+          subject: "Subject 1",
+          grade: "0",
+          units: "3",
+        },
+      ];
+    }
+  });
+  const [gwa, setGwa] = useState<number>(0);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    localStorage.setItem("grades", JSON.stringify(grades));
+  }, [grades]);
+
+  // Update grade value for a specific subject
+  const handleGradeChange = (index: number, value: number | string) => {
+    const updatedGrades = [...grades];
+    updatedGrades[index] = { ...updatedGrades[index], grade: value };
+    setGrades(updatedGrades);
+  };
+
+  // Update units value for a specific subject
+  const handleUnitsChange = (index: number, value: number | string) => {
+    const updatedGrades = [...grades];
+    updatedGrades[index] = { ...updatedGrades[index], units: value };
+    setGrades(updatedGrades);
+  };
+
+  // Add a new subject
+  const addSubject = () => {
+    const newSubject: Grade = {
+      subject: `Subject ${grades.length + 1}`,
+      grade: "0",
+      units: "3",
+    };
+    setGrades([...grades, newSubject]);
+  };
+
+  // Remove a subject
+  const removeSubject = (index: number) => {
+    if (grades.length === 1) {
+      setError("Cannot remove the last subject");
+      return;
+    }
+    const updatedGrades = [...grades];
+    updatedGrades.splice(index, 1);
+    setGrades(updatedGrades);
+  };
+
+  // Calculate the GWA
+  const calculateGwa = () => {
+    if (grades.length === 0) {
+      setError("Please add at least one subject");
+      return;
+    }
+
+    let totalGradePoints = 0;
+    let totalUnits = 0;
+
+    grades.forEach((grade) => {
+      totalGradePoints += Number(grade.grade) * Number(grade.units);
+      totalUnits += Number(grade.units);
+    });
+
+    const calculatedGwa = totalGradePoints / totalUnits;
+    setGwa(Number(calculatedGwa.toFixed(2))); // Limit to 2 decimal places
+    setError("");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="bg-main-blue overflow-hidden min-h-screen flex justify-center items-center flex-col">
+      <h1 className="text-secondary text-6xl font-bold uppercase mb-10 text-white">
+        GWA Calculator
+      </h1>
+      <div className="max-w-2xl mx-auto">
+        {grades.length <= 1 && error && (
+          <Alert variant="destructive" className="mb-4">
+            <ExclamationTriangleIcon className="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>Cannot remove the last subject.</AlertDescription>
+          </Alert>
+        )}
+        <button
+          className="bg-white rounded-t-xl text-2xl ml-auto block px-4 py-2 font-bold border-gray-500 border-b"
+          onClick={addSubject}
+        >
+          <PlusCircledIcon className="h-6 w-6" />
+        </button>
+        <Table>
+          <TableHeader className="bg-main-yellow">
+            <TableRow>
+              <TableHead className="text-main-blue font-bold">
+                Subject
+              </TableHead>
+              <TableHead className="text-main-blue font-bold">Grade</TableHead>
+              <TableHead className="text-main-blue font-bold">Units</TableHead>
+              <TableHead className="text-main-blue font-bold"></TableHead>{" "}
+              {/* Empty TableHead for Actions */}
+            </TableRow>
+          </TableHeader>
+          <TableBody className="bg-white">
+            {grades.map((grade, index) => (
+              <TableRow key={index}>
+                <TableCell>{grade.subject}</TableCell>
+                <TableCell>
+                  <input
+                    className="px-1 py-2"
+                    type="text"
+                    value={grade.grade}
+                    onChange={(e) => handleGradeChange(index, e.target.value)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <input
+                    className="px-1 py-2"
+                    type="text"
+                    value={grade.units}
+                    onChange={(e) => handleUnitsChange(index, e.target.value)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <button onClick={() => removeSubject(index)}>
+                    <MinusCircledIcon className="h-6 w-6" />
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <button
+          className="bg-white rounded-xl mt-4 w-full text-main-blue font-bold uppercase px-2 py-4 block text-xl"
+          onClick={calculateGwa}
+        >
+          Calculate
+        </button>
+        <p className="text-white font-bold uppercase text-3xl mt-4">
+          GWA: {gwa}
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
-  )
+  );
 }
